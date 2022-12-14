49643117c8e88709ec812f0cfed09c4d6d2aa16e
Samar <samarbadriddinov.tv@gmail.com>
2022-12-14 11:12:52 +0200

    #4. Token saqlash

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import {printError, printSuccess, printHelp} from './services/log.service.js'
 import getArgs from './helpers/args.js'
+import {printError, printSuccess, printHelp} from './services/log.service.js'
+import {saveKeyValue} from './services/storage.service.js'
+
+const saveToken = async token => {
+	try {
+		await saveKeyValue('token', token)
+		printSuccess('Token was saved')
+	} catch (error) {
+		printError(error.message)
+	}
+}
 
 const startCLI = () => {
 	const args = getArgs(process.argv)
@@ -11,7 +21,7 @@ const startCLI = () => {
 		// save city
 	}
 	if (args.t) {
-		// save token
+		return saveToken(args.t)
 	}
 	// result
 }
---
+++ services/storage.service.js
@@ -0,0 +1,38 @@
+import os from 'os'
+import path from 'path'
+import fs from 'fs'
+
+const filePath = path.join(os.homedir(), 'weather-data.json')
+
+const saveKeyValue = async (key, value) => {
+	let data = {}
+
+	if (await isExist(filePath)) {
+		const file = await fs.promises.readFile(filePath)
+		data = JSON.parse(file)
+	}
+
+	data[key] = value
+	await fs.promises.writeFile(filePath, JSON.stringify(data))
+}
+
+const getKeyValue = async key => {
+	if (await isExist(filePath)) {
+		const file = await fs.promises.readFile(filePath)
+		const data = JSON.parse(file)
+		return data[key]
+	}
+
+	return undefined
+}
+
+const isExist = async path => {
+	try {
+		await fs.promises.stat(path)
+		return true
+	} catch (error) {
+		return false
+	}
+}
+
+export {saveKeyValue, getKeyValue}
